57a8878f078d4c923a5da5aea641113d58247371
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-03 10:09:21 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - MATRICES M.06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EXERCISE_01___MATRICES_M._06 {
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            double[,] mat = new double[2, 3]; // ------ > Declação da matriz 
+
+            Console.WriteLine(mat.Length); // ------ > Tamanho da matriz
+            Console.WriteLine(mat.Rank); // ------ >  Tamanho da primeira dimensão
+            Console.WriteLine(mat.GetLength(0)); // ------ > Tamanho da PRIMEIRA dimensão da matriz 
+            Console.WriteLine(mat.GetLength(0)); // ------ > Tamanho da SEGUNDA dimensão da matriz 
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
